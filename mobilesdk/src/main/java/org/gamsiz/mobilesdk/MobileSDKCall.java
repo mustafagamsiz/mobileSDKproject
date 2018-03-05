c343018ff42d53f7c96388b102ccc0a170d7b54b
Mustafa Gamsiz <mgamsiz@netas.com.tr>
2018-03-05 15:07:46 +0300

    WebRTC version 1 usage.

--- mobilesdk/src/main/java/org/gamsiz/mobilesdk/MobileSDKCall.java
+++ mobilesdk/src/main/java/org/gamsiz/mobilesdk/MobileSDKCall.java
@@ -2,10 +2,21 @@ package org.gamsiz.mobilesdk;
 
 import android.util.Log;
 
+import org.gamsiz.webrtc.WebRTCCall;
+
 /**
  * Created by mgamsiz on 02/03/2018.
  */
 
 public class MobileSDKCall {
+    private WebRTCCall webRTCCall;
 
+    public MobileSDKCall() {
+        this.webRTCCall = new WebRTCCall();
+    }
+
+    public void establishCall() {
+        webRTCCall.establishCall();
+        Log.i("MobileSDKCall v1", "MobileSDKCall-v1 established");
+    }
 }
